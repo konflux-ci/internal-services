ce3e931c731efd8f9f601c0402b34d6b28362f6d
David Moreno García <damoreno@redhat.com>
2023-02-13 12:07:52 +0100

    feat(HACBS-1763): add GitHub workflow to automate the release
    This commit adds a workflow to populate the repository used to
    deploy resources to app-interface.
    
    Signed-off-by: David Moreno García <damoreno@redhat.com>

--- controllers/internalrequest/controller.go
+++ controllers/internalrequest/controller.go
@@ -56,10 +56,12 @@ func NewInternalRequestReconciler(client client.Client, remoteClient client.Clie
 	}
 }
 
-//+kubebuilder:rbac:groups=appstudio.redhat.com,resources=internalrequests,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=appstudio.redhat.com,resources=internalrequests/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=appstudio.redhat.com,resources=internalrequests/finalizers,verbs=update
-//+kubebuilder:rbac:groups=appstudio.redhat.com,resources=internalservicesconfigs,verbs=get
+// +kubebuilder:rbac:groups=appstudio.redhat.com,resources=internalrequests,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=appstudio.redhat.com,resources=internalrequests/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=appstudio.redhat.com,resources=internalrequests/finalizers,verbs=update
+// +kubebuilder:rbac:groups=appstudio.redhat.com,resources=internalservicesconfigs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=tekton.dev,resources=pipelineruns,verbs=get;list;watch;create
+// +kubebuilder:rbac:groups=tekton.dev,resources=pipelineruns/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
